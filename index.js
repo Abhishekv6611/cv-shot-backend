aa843d466396e2cff97e560bb4dbfbf96a1c5b7e
Abhishekv6611 <abiabhi810@gmail.com>
2025-04-09 14:10:32 +0530

    updated to normal form

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import ConnectDB from './lib/db.js'
+import RouterApp from './view/authView.js' 
+dotenv.config()
+const app=express()
+
+const PORT=process.env.PORT || 5050
+app.use(cors())
+app.use(express.json())
+
+
+ConnectDB()
+app.use(RouterApp)
+
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+    
+})
