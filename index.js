dfcd141080220505aa3c77f8ac30ea54003ec9f5
Abhishekv6611 <abiabhi810@gmail.com>
2025-04-09 09:45:05 +0530

    changed to serveless

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import ConnectDB from './lib/db.js'
 import RouterApp from './view/authView.js' 
+import ServerlessHttp from 'serverless-http'
 dotenv.config()
 const app=express()
 
@@ -14,7 +15,12 @@ app.use(express.json())
 ConnectDB()
 app.use(RouterApp)
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-    
-})
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5050
+    app.listen(PORT, () => {
+      console.log(`Server is running locally on port ${PORT}`)
+    })
+  }
+
+
+export const handler = ServerlessHttp(app); 
