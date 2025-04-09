e04b1b0e9ea9d2285fd12796905a2a4ebcd2d257
Abhishekv6611 <abiabhi810@gmail.com>
2025-04-09 09:54:02 +0530

    added new index.js

--- app/index.js
+++ app/index.js
@@ -1,25 +1,43 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import ConnectDB from '../lib/db.js'
-import RouterApp from '../view/authView.js' 
-import ServerlessHttp from 'serverless-http'
-dotenv.config()
-const app=express()
+// import express from 'express'
+// import cors from 'cors'
+// import dotenv from 'dotenv'
+// import ConnectDB from '../lib/db.js'
+// import RouterApp from '../view/authView.js' 
+// import ServerlessHttp from 'serverless-http'
+// dotenv.config()
+// const app=express()
 
-app.use(cors())
-app.use(express.json())
-
-
-ConnectDB()
-app.use(RouterApp)
-
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 5050
-    app.listen(PORT, () => {
-      console.log(`Server is running locally on port ${PORT}`)
-    })
-  }
+// app.use(cors())
+// app.use(express.json())
 
 
-export const handler = ServerlessHttp(app); 
+// ConnectDB()
+// app.use(RouterApp)
+
+// if (process.env.NODE_ENV !== 'production') {
+//     const PORT = process.env.PORT || 5050
+//     app.listen(PORT, () => {
+//       console.log(`Server is running locally on port ${PORT}`)
+//     })
+//   }
+
+
+// export const handler = ServerlessHttp(app); 
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import ConnectDB from '../lib/db.js';
+import RouterApp from '../view/authView.js';
+import serverless from 'serverless-http';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+ConnectDB();
+app.use(RouterApp);
+
+export const handler = serverless(app);
+export default app;
