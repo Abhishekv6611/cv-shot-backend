e1d085013e1e6397b2b2ebff97fe9f47bf718ae9
Abhishekv6611 <abiabhi810@gmail.com>
2025-04-09 13:20:35 +0530

    changed to prev code on indexjs

--- app/index.js
+++ app/index.js
@@ -1,46 +1,46 @@
-// import express from 'express'
-// import cors from 'cors'
-// import dotenv from 'dotenv'
-// import ConnectDB from '../lib/db.js'
-// import RouterApp from '../view/authView.js' 
-// import ServerlessHttp from 'serverless-http'
-// dotenv.config()
-// const app=express()
-
-// app.use(cors())
-// app.use(express.json())
-
-
-// ConnectDB()
-// app.use(RouterApp)
-
-// if (process.env.NODE_ENV !== 'production') {
-//     const PORT = process.env.PORT || 5050
-//     app.listen(PORT, () => {
-//       console.log(`Server is running locally on port ${PORT}`)
-//     })
-//   }
-
-
-// export const handler = ServerlessHttp(app); 
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-// import ConnectDB from '../lib/db.js';
-import RouterApp from '../view/authView.js';
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import ConnectDB from '../lib/db.js'
+import RouterApp from '../view/authView.js' 
 import serverless from 'serverless-http';
+dotenv.config()
+const app=express()
 
-dotenv.config();
+app.use(cors())
+app.use(express.json())
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+
+ConnectDB()
+app.use(RouterApp)
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5050
+    app.listen(PORT, () => {
+      console.log(`Server is running locally on port ${PORT}`)
+    })
+  }
+
+
+  export default serverless(app); 
+// import express from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+// import ConnectDB from '../lib/db.js'
+// import RouterApp from '../view/authView.js';
+// import serverless from 'serverless-http';
+
+// dotenv.config();
+
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
 // ConnectDB();
-app.use('/auth',RouterApp);
+// app.use('/auth',RouterApp);
 
-app.get('/home',(req,res)=>{
-  res.json({message:"Home"})
-})
+// app.get('/home',(req,res)=>{
+//   res.json({message:"Home"})
+// })
 
-export default serverless(app);
+// export default serverless(app);
